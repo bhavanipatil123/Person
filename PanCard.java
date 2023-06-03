934aafe0654e4f5ed5bc690fd831d8a4ed93ca6e
bhavanipatil123 <bhavanipatil1997@gmail.com>
2023-06-03 13:06:41 +0530

    myfirstcommit

---
+++ PanCard.java
@@ -0,0 +1,35 @@
+package com.ty.project1;
+
+public class PanCard {
+	private int id;
+	private String name;
+	private String add;
+	private long phno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	
+	}
+
+
